75579b77fdffa49a6214468712751e9349310aca
John Dunning <fw@johndunning.com>
2025-03-01 16:49:06 -0800

    Handle draft versions of minutes
    Support passing paths or IDs in the folders array for GoogleDriveAPI.
    Handle exceptions thrown in createLoader() and prevent the minutes list from showing if none are found.
    Rename BOARD_MINUTES_DRIVE_PATH to BOARD_MINUTES_DRIVE_FOLDER.
    Change preprocess to a function that receives the full files array and move filtering of minutes files to preprocess().
    Use GoogleDriveAPI in createLoader() and clean up GoogleDriveAPI.ts.
    Add GoogleDriveAPI class.
    Remove .env listing from deploy.yml.
    Eagerly load team member images.
    Add spacing above anchors used as buttons.
    Update Astro to v5.4.1.

--- src/content/minutes/collection.ts
+++ src/content/minutes/collection.ts
@@ -1,41 +1,55 @@
 import { defineCollection, z } from "astro:content";
+import type { drive_v3 } from "googleapis";
 import { createLoader } from "./loader.ts";
-import type { PDF } from "./gdrive";
 
-const QuarterPattern = /^(\d{4})[- _]+Q(\d)/;
+const MinutesPDFPattern = /(\d{4})[- _]+Q(\d)/;
+const DraftPDFPattern = /^DRAFT\s+(\d{4})[- _]+Q(\d)/i;
+// strip this unnecessary param from the viewLinks
+const USPPattern = /\?usp=\w+$/;
 
+const credentials = JSON.parse(import.meta.env.DRIVE_KEYS_JSON ?? "");
 const driveID = import.meta.env.BOARD_MINUTES_DRIVE_ID ?? "";
 // the path should be from the root of the shared drive to the folder containing
 // the PDFs of the minutes
-const minutesPath = import.meta.env.BOARD_MINUTES_DRIVE_PATH ?? "";
+const minutesFolder = import.meta.env.BOARD_MINUTES_DRIVE_FOLDER ?? "";
+
+const MinutesSchema = z.object({
+	id: z.string(),
+	name: z.string(),
+	createdTime: z.string(),
+	url: z.string(),
+	year: z.number(),
+	quarter: z.number(),
+	isDraft: z.boolean().default(false),
+});
 
 async function preprocess(
-	pdf: PDF)
+	files: drive_v3.Schema$File[])
 {
-		const { name } = pdf;
-		const result = name?.match(QuarterPattern);
-		const year = parseInt(result?.[1] ?? "");
-		const quarter = parseInt(result?.[2] ?? "");
+	// use flatMap() so we can filter out files we want to skip without TS
+	// complaining that the array might contain nulls
+	return files.flatMap((file) => {
+		const result = file.name?.match(MinutesPDFPattern);
 
-		if (isNaN(year) || isNaN(quarter)) {
-			return null;
+		if (!result || file.mimeType !== "application/pdf") {
+			return [];
 		}
 
+		const year = parseInt(result[1]);
+		const quarter = parseInt(result[2]);
+		const isDraft = DraftPDFPattern.test(file.name ?? "");
+
 		return {
-			...pdf,
+			...file,
 			year,
 			quarter,
+			isDraft,
+			url: file.webViewLink?.replace(USPPattern, "") ?? "",
 		};
+	});
 }
 
 export const minutes = defineCollection({
-	loader: createLoader("minutes", driveID, minutesPath, preprocess),
-	schema: z.object({
-		id: z.string(),
-		name: z.string(),
-		createdTime: z.string(),
-		url: z.string(),
-		year: z.number(),
-		quarter: z.number(),
-	})
+	loader: createLoader("minutes", credentials, driveID, [minutesFolder], preprocess),
+	schema: MinutesSchema
 });
--- src/content/minutes/loader.ts
+++ src/content/minutes/loader.ts
@@ -1,12 +1,16 @@
 import type { Loader } from "astro/loaders";
-import { getPDFsInFolder, type PDF } from "./gdrive.ts";
+import type { drive_v3 } from "googleapis";
+import { GoogleDriveAPI } from "./GoogleDriveAPI.ts";
 
 export function createLoader(
 	name: string,
+	credentials: object,
 	driveID: string,
-	path: string,
-	preprocess?: (data: PDF) => Promise<PDF | null>): Loader
+	folders: string[],
+	preprocess?: (files: drive_v3.Schema$File[]) => Promise<drive_v3.Schema$File[]>): Loader
 {
+	const drive = new GoogleDriveAPI({ driveID, credentials });
+
 	return {
 		name,
 		async load({
@@ -15,37 +19,48 @@ export function createLoader(
 			parseData,
 			logger })
 		{
-			const pageToken = meta.get("pageToken");
-			let { foundChanges, newPageToken, pdfs } =
-				await getPDFsInFolder(driveID, path, pageToken);
+			try {
+				const pageToken = meta.get("pageToken");
+				const { changed, newPageToken } = await drive.foldersHaveChanged(folders, pageToken);
 
-			if (typeof preprocess === "function") {
-				// this may throw and stop the build, but it's not really clear how
-				// errors should be handled here, so leave it for now
-				pdfs = (await Promise.all(pdfs.map(preprocess)))
-					.filter(Boolean) as PDF[];
-			}
+				// store the new page token in the build cache so that the next time the
+				// content collection loader is called, we can use the token to check if
+				// anything actually changed
+				meta.set("pageToken", newPageToken);
 
-			// store the new page token in the build cache so that the next time the
-			// content collection loader is called, we can use the token to check if
-			// anything actually changed
-			meta.set("pageToken", newPageToken);
+				if (changed) {
+					// clear the store if there are any changes, since getPDFsInFolder()
+					// returns all the PDFs currently in the folder, and it's easier to
+					// just replace everything in order to handle deletions
+					store.clear();
 
-			if (foundChanges) {
-				// clear the store if there are any changes, since getPDFsInFolder()
-				// returns all the PDFs currently in the folder, and it's easier to
-				// just replace everything in order to handle deletions
-				store.clear();
-				logger.info(`Changes found, ${pdfs.length} PDFs`);
+					let files = await drive.getFiles({ folders });
 
-				for (const pdf of pdfs) {
-					const { id } = pdf;
-					// parseData() uses the schema defined in the collection to validate
-					// the data
-					const data = await parseData({ id, data: pdf });
+					if (typeof preprocess === "function") {
+						// this may throw and stop the build, but it's not really clear how
+						// errors should be handled here, so leave it for now
+						files = await preprocess(files);
+					}
 
-					store.set({ id, data });
+					logger.info(`Stored ${files.length} files`);
+
+					for (const file of files) {
+						// for some reason, the id property in Schema$File is typed as being
+						// possibly null, even though that never seems to happen.  so default
+						// to an empty string to keep TS happy.
+						const id = file.id || "";
+						// parseData() uses the schema defined in the collection to validate
+						// the data, and requires that all of the property keys are strings,
+						// so cast the file, since Schema$File isn't typed the right way
+						const data = await parseData(
+							{ id, data: file as Record<string, unknown> });
+
+						store.set({ id, data });
+					}
 				}
+			} catch (err) {
+				logger.error(String(err));
+				logger.error(`Content collection will be empty.`);
 			}
 		}
 	};
